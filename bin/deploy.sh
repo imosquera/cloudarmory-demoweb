30c5687cb752867014eb28cb52eb88662468ad94
Isaac Mosquera <imosquer@gmail.com>
2016-05-16 10:09:53 -0700

    initial commit of the website

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,9 +2,23 @@
 cd  `dirname $0`/../
 BASEDIR=$(pwd)
 
-kubectl create secret generic web-html \
-    --from-file=build/html/ \
+
+echo "REMOVEING SECRETS AND PODS"
+kubectl delete secret armory-web-html --namespace=${NAMESPACE}
+kubectl delete secret armory-web-nginx-config --namespace=${NAMESPACE}
+kubectl delete svc,pod -l stack=web --namespace=${NAMESPACE}
+
+
+echo "ADDING SECRETS AND PODS"
+kubectl create secret generic armory-web-html \
+    --from-file=html/ \
     --namespace=${NAMESPACE}
 
-kubectl cre
+kubectl create secret generic armory-web-nginx-config \
+    --from-file=etc/web-nginx.conf \
+    --namespace=${NAMESPACE}
+
+kubectl create -f etc/kubernetes/web-pod.yaml --namespace=${NAMESPACE}
+kubectl create -f etc/kubernetes/web-svc.yaml --namespace=${NAMESPACE}
+
 #$ docker run -d -P --name web -v /src/webapp:/opt/webapp training/webapp python app.py
